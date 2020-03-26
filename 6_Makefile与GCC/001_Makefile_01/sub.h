450127580d7b8a57fad5e24eed482b1151f5ac6b
codebug8 <livelwh@outlook.com>
2020-03-26 11:57:33 +0800

    增加第六章Makefile与GCC章节配套源码
    Signed-off-by: codebug8 <livelwh@outlook.com>

---
+++ 6_Makefile与GCC/001_Makefile_01/sub.h
@@ -0,0 +1,6 @@
+#ifndef __SUB_H
+#define __SUB_H
+
+int sub(int a, int b);
+
+#endif 
---
+++ 6_Makefile与GCC/001_gcc_02/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define HUNDRED 100
+
+int main()
+{
+	int a = 0;
+	printf("%d ask\n",HUNDRED);
+	return 0;
+}
